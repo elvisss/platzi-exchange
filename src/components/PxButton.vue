5d96a5b7f8ec4cf1d88251e1e37ac341a66809d1
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2020-09-22 12:59:48 -0500

    adding routing

---
+++ src/components/PxButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button
+    @click="onClick"
+    class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-2 border border-green-500 hover:border-transparent rounded"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'PxButton',
+  methods: {
+    onClick() {
+      this.$emit('custom-click')
+    }
+  }
+}
+</script>
